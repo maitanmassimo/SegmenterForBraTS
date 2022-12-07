63977f1447890870ddbdca775305e1c56f647646
Massimo <massimo.maitan@mail.polimi.it>
2022-12-07 16:49:40 +0100

    debugging metrics

--- segm/metrics.py
+++ segm/metrics.py
@@ -90,6 +90,11 @@ def compute_metrics(
     tmp_dir=None,
     distributed=False,
 ):
+    #print("Seg prediction:")
+    #print(seg_pred)
+    #print("Seg ground thruth")
+    #print(seg_gt)
+
     ret_metrics_mean = torch.zeros(3, dtype=float, device=ptu.device)
     if ptu.dist_rank == 0:
         list_seg_pred = []
