9fd176e36d93159cd3ce9e1ceafa777327eed11d
Massimo <massimo.maitan@mail.polimi.it>
2022-12-08 16:15:28 +0100

    debugging logger for cat_iou

--- segm/utils/logger.py
+++ segm/utils/logger.py
@@ -87,15 +87,15 @@ class MetricLogger(object):
 
     def update(self, n=1, **kwargs):
         for k, v in kwargs.items():
-            #print("Logger")
-            #print("k: {}".format(k))
-            #print("k type: {}".format(type(k)))
-            #print("v: {}".format(v))
-            #print("v type: {}".format(type(v)))
             if isinstance(v, torch.Tensor):
                 v = v.item()
             if isinstance(v, np.ndarray):
                 v = v.tostring()
+            print("Logger")
+            print("k: {}".format(k))
+            print("k type: {}".format(type(k)))
+            print("v: {}".format(v))
+            print("v type: {}".format(type(v)))
             assert isinstance(v, (float, int, str))
             self.meters[k].update(v, n)
 
