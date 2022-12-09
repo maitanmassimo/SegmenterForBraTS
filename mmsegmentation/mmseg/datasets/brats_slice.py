779ea938ef1c0bd841a8367bcae46980e79b0629
Massimo <massimo.maitan@mail.polimi.it>
2022-12-09 15:53:53 +0100

    reduce_zero_label = false in bratsSliceDataset in mmsegmentation

--- mmsegmentation/mmseg/datasets/brats_slice.py
+++ mmsegmentation/mmseg/datasets/brats_slice.py
@@ -30,7 +30,7 @@ class BratsSliceDataset(CustomDataset):
         super(BratsSliceDataset, self).__init__(
             img_suffix='.png',
             seg_map_suffix='.png',
-            reduce_zero_label=True,
+            reduce_zero_label=False,
             **kwargs)
 
     def results2img(self, results, imgfile_prefix, to_label_id):
