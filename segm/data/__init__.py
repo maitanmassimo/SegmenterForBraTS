59c1e0f31e0ac2fe6cfcb5a7d3d836ee6db593a2
Massimo <massimo.maitan@mail.polimi.it>
2023-09-03 19:05:33 +0200

    corretto import di BratsSliceWTDataset in segm/data/init.py

--- segm/data/__init__.py
+++ segm/data/__init__.py
@@ -5,4 +5,4 @@ from segm.data.ade20k import ADE20KSegmentation
 from segm.data.pascal_context import PascalContextDataset
 from segm.data.cityscapes import CityscapesDataset
 from segm.data.brats_slice import BratsSliceDataset
-from segm.data.brats_slice import BratsSliceWTDataset
+from segm.data.brats_slice_wt import BratsSliceWTDataset
