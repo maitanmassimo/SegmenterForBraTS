06574076886c6753b856969a4975a0ea7aae562b
Massimo <massimo.maitan@mail.polimi.it>
2023-11-13 20:15:44 +0100

    testing consequent evaluation

--- segm/eval/cons_miou_per_patient.py
+++ segm/eval/cons_miou_per_patient.py
@@ -211,7 +211,7 @@ def eval_dataset(
     for n in range(len(seg_gt_maps)):
         if n%128 == 127 or n%128 == 126:
             print(f"FILTERING OUT {n} : image {n+2}")
-            print(f"deleting image {next(itertools.islice(filtered_total_seg_pred_maps,n,None)) +2}")
+            print(f"deleting image {str(next(itertools.islice(filtered_total_seg_pred_maps,n,None)) +2)}")
             del filtered_total_seg_pred_maps[next(itertools.islice(filtered_total_seg_pred_maps,n,None))]
             del filtered_total_seg_gt_maps[next(itertools.islice(filtered_total_seg_gt_maps,n,None))]
 
